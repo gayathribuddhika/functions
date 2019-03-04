65d8fabbe8e28593958bbc7b88e6dd9db35ef03f
[gayathribuddhika] <[wg.gayathri@gmail.com]>
2019-03-04 09:55:20 +0530

    Functions
    Functios in Python

---
+++ functions.py
@@ -0,0 +1,14 @@
+def beef():
+    print("Hello, Functions are easy to Understand")
+
+def bitcoin_to_usd(btc):
+    amount = btc * 527
+    print(amount)
+
+beef()
+beef()
+beef()
+print(" ")
+bitcoin_to_usd(1)
+bitcoin_to_usd(4.25)
+bitcoin_to_usd(3.11)
